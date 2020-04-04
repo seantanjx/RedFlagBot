05a750c18bf5e1d7a7b317370400d85a640f89a9
Sean Tan <48247283+KopiRoasters@users.noreply.github.com>
2020-04-05 01:34:11 +0800

    Delete twitter_scrape.py

--- twitter_scrape.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import requests
-
-account_user = 'dadsaysjokes'
-
-source = requests.get("https://twitter.com/" + account_user, timeout=5).text
-soup = bs(source, 'lxml')
-print(soup.prettify())
